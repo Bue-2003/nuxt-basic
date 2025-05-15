345c330bf805d2505ec746d38032f2e91fc71a0b
Bue <myatbhonemaw72@gmail.com>
2025-05-15 20:15:00 +0630

    De-dupe Async Data with a Key

---
+++ pages/movies/[id].vue
@@ -0,0 +1,16 @@
+<script setup>
+    const route = useRoute();
+    const { data } = useAsyncData(`/movies/${route.params.id}`,() => {
+        return $fetch(
+            `http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`
+        );
+    })
+</script>
+
+<template>
+    <div>
+        <h1>
+            {{ data }}
+        </h1>
+    </div>
+</template>
