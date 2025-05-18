69d5e09354a7c50ba5e495825a29b96539be4db5
Bue <myatbhonemaw72@gmail.com>
2025-05-18 22:17:05 +0630

    image_modifiers

--- pages/responsive.vue
+++ pages/responsive.vue
@@ -1,16 +1,45 @@
-<template>
-  <NuxtImg src="/cat.jpg" sizes="xs:100vw sm:50vw md:1860px" />
-</template>
-
 <script setup>
 definePageMeta({
-    layout : "plain"
-})
+  layout: "plain",
+});
+const modifiers = ref({
+  quality: 100,
+  flip: undefined,
+  flop: undefined,
+  sharpen: undefined,
+  blur: undefined,
+  negate: undefined,
+  tint: undefined,
+  grayscale: undefined,
+});
+
+const crop = ref({
+  left: 0,
+  top: 0,
+  width: 0,
+  height: 0,
+});
+
+const extract = computed(() => {
+  if (crop.value.width && crop.value.height)
+    return `${crop.value.left}_${crop.value.top}_${crop.value.width}_${crop.value.height}`;
+  return;
+});
 </script>
+<template>
+  <NuxtImg
+    src="/cat.webp"
+    sizes="xs:100vw sm:50vw md:1860px"
+    :modifiers="{
+      ...modifiers,
+      extract: extract,
+    }"
+  />
+</template>
 
 <style scoped>
-img{
-    display: block;
-    max-width: 100%;
+img {
+  display: block;
+  max-width: 100%;
 }
-</style>    
+</style>
