c16bf8d7f277a66351ff1fcaf19f1fa18c4aaa13
Bue <myatbhonemaw72@gmail.com>
2025-05-18 11:58:53 +0630

    Global State Management with useState

--- pages/login.vue
+++ pages/login.vue
@@ -1,17 +1,28 @@
 <script setup>
+
+const isLoggedIn = useIsLoggedIn();
 definePageMeta({
     layout : "plain",
 })
+
+function login() {
+    isLoggedIn.value = true;
+    useRouter().push("/")
+}
 </script>
 
 <template>
     <div>
         <h2>Login</h2>
-        <label>Username
+        <form @submit.prevent="login">
+            <label>Username
             <input type="text">
         </label>
         <label>Password
             <input type="text">
         </label>
+        <button>login</button>
+        </form>
+       
     </div>
 </template>
