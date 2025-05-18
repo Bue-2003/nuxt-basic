78c45348f88ebf30f7b1c5103e1c7ee761e9cdda
Bue <myatbhonemaw72@gmail.com>
2025-05-18 10:16:50 +0630

    create_flexible_custom_layouts

---
+++ pages/login.vue
@@ -0,0 +1,17 @@
+<script setup>
+definePageMeta({
+    layout : "plain",
+})
+</script>
+
+<template>
+    <div>
+        <h2>Login</h2>
+        <label>Username
+            <input type="text">
+        </label>
+        <label>Password
+            <input type="text">
+        </label>
+    </div>
+</template>
