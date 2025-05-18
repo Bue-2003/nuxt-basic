256d583e895d41127f4f470b1c4e7d41db310b56
Bue <myatbhonemaw72@gmail.com>
2025-05-18 22:44:31 +0630

    The_NuxtPicture_components

--- pages/responsive.vue
+++ pages/responsive.vue
@@ -1,4 +1,5 @@
-<script setup>
+<!-- image modifier -->
+<!-- <script setup>
 definePageMeta({
   layout: "plain",
 });
@@ -42,4 +43,56 @@ img {
   display: block;
   max-width: 100%;
 }
+</style> -->
+<!-- //end modifier -->
+
+
+
+<!-- The NuxtPicture component -->
+<script setup>
+definePageMeta({
+  layout: "plain",
+});
+const modifiers = ref({
+  quality: 100,
+  flip: undefined,
+  flop: undefined,
+  sharpen: undefined,
+  blur: undefined,
+  negate: undefined,
+  tint: undefined,
+  grayscale: undefined,
+});
+
+const crop = ref({
+  left: 0,
+  top: 0,
+  width: 0,
+  height: 0,
+});
+
+const extract = computed(() => {
+  if (crop.value.width && crop.value.height)
+    return `${crop.value.left}_${crop.value.top}_${crop.value.width}_${crop.value.height}`;
+  return;
+});
+</script>
+<template>
+  <NuxtPicture
+    format="avif"
+    src="/cat.webp"
+    sizes="xs:100vw sm:50vw md:1860px"
+    :modifiers="{
+      ...modifiers,
+      extract: extract,
+    }"
+  />
+</template>
+
+<style scoped>
+:deep(img) {
+  display: block;
+  max-width: 100%;
+}
 </style>
+<!-- End NuxtPicture component -->
